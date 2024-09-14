dcb2795db265aed640f0492997f0d38c5e9e863b
katzhukk <katzhukova230@gmail.com>
2024-09-14 23:06:56 +0300

    add SelenideSearch.java

--- src/test/java/SelenideSearch.java
+++ src/test/java/SelenideSearch.java
@@ -10,13 +10,14 @@ public class SelenideSearch {
     static void beforeAll() {/* Всегда пишется со static. Вызывается один перед всеми тестами в этом тестовом классе*/
         Configuration.browserSize = "1920x1080";        /* Задаем разрешение браузера */
         Configuration.pageLoadStrategy = "eager";       /* Не ждем, когда загрузится полностью страница, чтобы долго не ждать*/
+        Configuration.baseUrl = "https://github.com/";  /* Открывает основную страницу сайта */
         //Configuration.holdBrowserOpen = true;         /* Не дает закрыть тесту браузер. Нужно только для отладки */
     }
 
     @Test
     void fillFormTest() {
         // открыть главную страницу selenide/selenide в github
-        open("https://github.com/selenide/selenide");
+        open("/selenide/selenide");
         // перейти в раздел Wiki проекта
         $("#wiki-tab").click();
         // убедиться, что в списке страниц (Pages) есть страница SoftAssertions
