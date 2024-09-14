193f14a285913b1a328f2303324d9ef72dc8a369
katzhukk <katzhukova230@gmail.com>
2024-09-14 22:30:40 +0300

    add SelenideSearch.java

--- src/test/java/SelenideSearch.java
+++ src/test/java/SelenideSearch.java
@@ -1,12 +1,8 @@
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.WebDriverRunner;
-import com.codeborne.selenide.selector.ByText;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideSearch {
